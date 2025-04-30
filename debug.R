7ee431983d9f095cf283c205a927d7db652ade9f
martiepe <martiepe@stud.ntnu.no>
2025-04-30 12:52:15 +0200

    commit

--- debug.R
+++ debug.R
@@ -1,100 +1,3 @@
-# library(Rhabit)
-# library(raster)
-source("functions/utility_functions.R")
-sourceDir("functions") 
-load_lib(ggplot2, viridis, reshape2, gridExtra, mvtnorm, foreach, 
-  iterators, parallel, doParallel, mvtnorm, ambient, mvnfast, optimParallel)
-# library(ggplot2)
-# library(viridis)
-# library(reshape2)
-# library(gridExtra)
-# library(mvtnorm)
-# library(foreach)
-# library(iterators)
-# library(parallel)
-# library(doParallel)
-# library(mvtnorm)
-# library(ambient)
-# library(mvnfast)
-# library(optimParallel)
-
-#perlin covariates
-lim <- c(-1, 1, -1, 1)*100
-resol <- 1
-ncov <- 2
-covlist <- list()
-xgrid <- seq(lim[1], lim[2], by = resol)
-ygrid <- seq(lim[3], lim[4], by = resol)
-coords <- as.matrix(expand.grid(xgrid, ygrid))
-for(i in 1:ncov) {
-  vals = 3*noise_perlin(c(length(xgrid), length(ygrid)), frequency = 0.05)
-  covlist[[i]] = list(x = xgrid, y = ygrid, z = matrix(vals, nrow = length(xgrid)))
-}
-# Include squared distance to centre of map as covariate
-xgrid <- seq(lim[1], lim[2], by=resol)
-ygrid <- seq(lim[3], lim[4], by=resol)
-xygrid <- expand.grid(xgrid,ygrid)
-dist2 <- ((xygrid[,1])^2+(xygrid[,2])^2)/(100)
-covlist[[3]] <- list(x=xgrid, y=ygrid, z=matrix(dist2, length(xgrid), length(ygrid)))
-
-
-
-
-
-
-bilinearGradVec <- function(loc_mat, cov_list) {
-  x_grid <- cov_list[[1]]$x
-  y_grid <- cov_list[[1]]$y
-  n_cov <- length(cov_list)
-  n_obs <- nrow(loc_mat)
-  
-  ix <- findInterval(loc_mat[,1], x_grid)
-  iy <- findInterval(loc_mat[,2], y_grid)
-  
-  valid <- ix > 0 & ix < length(x_grid) & iy > 0 & iy < length(y_grid)
-  
-  x1 <- x_grid[ix]
-  x2 <- x_grid[ix + 1]
-  y1 <- y_grid[iy]
-  y2 <- y_grid[iy + 1]
-  
-  dx <- x2 - x1
-  dy <- y2 - y1
-  lx <- loc_mat[,1]
-  ly <- loc_mat[,2]
-  
-  grad_array <- array(NA_real_, dim = c(n_cov, n_obs, 2))
-  
-  for (j in seq_len(n_cov)) {
-    f11 <- cov_list[[j]]$z[cbind(ix,     iy)]
-    f21 <- cov_list[[j]]$z[cbind(ix + 1, iy)]
-    f12 <- cov_list[[j]]$z[cbind(ix,     iy + 1)]
-    f22 <- cov_list[[j]]$z[cbind(ix + 1, iy + 1)]
-    
-    dfdx <- ((y2 - ly) * (f21 - f11) + (ly - y1) * (f22 - f12)) / (dy * dx)
-    dfdy <- ((x2 - lx) * (f12 - f11) + (lx - x1) * (f22 - f21)) / (dy * dx)
-    
-    grad_array[j, valid, 1] <- dfdx[valid]
-    grad_array[j, valid, 2] <- dfdy[valid]
-  }
-  
-  grad_array
-}
-
-
-
-#number of tracks to be generated
-ntrack <- 1
-#speed parameter for Langevin model
-speed <- 5
-
-
-
-set.seed(123)
-
-
-
-params = matrix(NA, ncol = 5, nrow = 5*100)
 for (ik in 1:100) {
   for (jk in 1:5) {
     beta <- c(4,2,-0.1)
@@ -257,8 +160,7 @@ for (ik in 1:100) {
     
     #using paralellized and vectorized likelihood in optim
     cl <- makeCluster(16)
-    clusterExport(cl, c("X", "M", "N", "delta", "P", "B", "Grad", "gradArray", "dmvn",  "lik_grad", 
-      "bilinearGradVec"))
+    clusterExport(cl, c("X", "M", "N", "delta", "P", "B", "Grad", "gradArray", "dmvn",  "lik_grad", "bilinearGradArray"))
     
     o = optim(par = c(0,0,0,1), fn = function(x) lik_grad(x, cl)$l, gr = function(x) lik_grad(x, cl)$g, method = "L-BFGS-B")
     
@@ -269,44 +171,9 @@ for (ik in 1:100) {
     params[ik*5+jk-5, 5] = N
   }
   
-  df = data.frame(beta1 = params[,1], beta2 = params[,2], beta3 = params[,3], gammasq = params[,4], M = as.factor(params[,5]))
+  df = data.frame(beta1 = params[,1], beta2 = params[,2], beta3 = params[,3], gammasq = params[,4], M = params[5])
   save(df,file="varying_M_estimates.Rda")
   
   
   print(ik)
 }
-
-
-p1 <- ggplot(data = df, aes(x = M, y = beta1)) +
-  geom_boxplot() +
-  geom_hline(yintercept  = 4, color = "red", linetype = 2) +
-  labs(title = "Beta_1") +
-  theme_bw()
-
-p2 <- ggplot(data = df, aes(x = M, y = beta2)) +
-  geom_boxplot() +
-  geom_hline(yintercept  = 2, color = "red", linetype = 2) +
-  labs(title = "Beta_2") +
-  theme_bw()
-
-p3 <- ggplot(data = df, aes(x = M, y = beta3)) +
-  geom_boxplot() +
-  geom_hline(yintercept  = -0.1, color = "red", linetype = 2) +
-  labs(title = "Beta_3") +
-  theme_bw()
-
-p4 <- ggplot(data = df, aes(x = M, y = gammasq)) +
-  geom_boxplot() +
-  geom_hline(yintercept  = 5, color = "red", linetype = 2) +
-  labs(title = "gamma^2") +
-  theme_bw()
-
-grid.arrange(p1,p2,p3,p4)
-
-
-
-
-
-
-
-
--- functions/Rhabit_functions.R
+++ functions/Rhabit_functions.R
@@ -154,3 +154,123 @@ getGridZoom <- function (covar, x0, lag_inter = 2)
 }
 
 
+# bilinearGradArray from Rhabit
+bilinearGradArray <- function (locs, cov_list) {
+  if (!inherits(locs, "matrix")) 
+    stop("'locs' must be a matrix")
+  grad <- unlist(lapply(1:nrow(locs), function(i) bilinearGrad(loc = locs[i, 
+    ], cov_list = cov_list)))
+  gradarray <- array(grad, c(2, length(cov_list), nrow(locs)))
+  gradarray <- aperm(gradarray, c(3, 1, 2))
+  return(gradarray)
+}
+
+#langevinUD
+langevinUD <- function (locs, times, ID = NULL, grad_array, with_speed = TRUE, 
+  alpha = 0.95, leverage = FALSE) 
+{
+  if (!(inherits(locs, "matrix") & typeof(locs) %in% c("double", 
+    "integer"))) 
+    stop("locs must be a numeric matrix")
+  if (inherits(grad_array, "matrix")) {
+    grad_array <- array(grad_array, dim = c(n, 2, 1))
+    warning("gradsArray was a matrix, and has been transformed to an array")
+  }
+  n <- nrow(locs)
+  if (is.null(ID)) 
+    ID <- rep(1, n)
+  if (length(times) != n) 
+    stop("Length of times must be the nrow of locs")
+  if (length(ID) != n) 
+    stop("Length of ID must be the nrow of locs")
+  if (dim(grad_array)[1] != n) 
+    stop("The first dimension of gradientArray must be of size nrow(locs)")
+  J <- dim(grad_array)[3]
+  break_ind <- which(ID[-1] != ID[-n])
+  start_ind <- c(1, break_ind + 1)
+  end_ind <- c(break_ind, n)
+  grad_mat <- 0.5 * rbind(grad_array[-end_ind, 1, ], grad_array[-end_ind, 
+    2, ])
+  time_lag <- rep(times[-start_ind] - times[-end_ind], 2)
+  sq_time_lag <- sqrt(time_lag)
+  loc_increment <- as.numeric(locs[-start_ind, ] - locs[-end_ind, 
+    ])
+  Y <- loc_increment/sq_time_lag
+  nu_hat_var <- solve(t(grad_mat * time_lag) %*% grad_mat)
+  DF <- length(Y) - J
+  if (DF <= 4) {
+    stop("nrow(locs) must be strictly larger than 2 + dim(grad_array)[3] / 2")
+  }
+  nu_hat <- nu_hat_var %*% t(grad_mat) %*% loc_increment
+  predictor <- sq_time_lag * grad_mat %*% nu_hat
+  if (with_speed) {
+    gamma2_hat <- colSums((Y - predictor)^2)/DF
+    beta_hat <- nu_hat/gamma2_hat * (DF - 2)/DF
+    beta_hat_var <- (2 * beta_hat %*% t(beta_hat)/(DF - 
+      4) + nu_hat_var/gamma2_hat * (1 + 2/(DF - 4)))
+  }
+  else {
+    beta_hat <- nu_hat
+    beta_hat_var <- nu_hat_var
+  }
+  quantlow <- (1 - alpha)/2
+  quantup <- 1 - (1 - alpha)/2
+  conf_interval_beta <- t(sapply(1:length(beta_hat), function(j) {
+    beta_hat[j] + c(1, -1) * stats::qnorm(quantlow) * sqrt(beta_hat_var[j, 
+      j])
+  }))
+  conf_interval_gamma2 <- gamma2_hat * DF/stats::qchisq(c(quantup, 
+    quantlow), DF)
+  conf_interval <- rbind(conf_interval_beta, conf_interval_gamma2)
+  rownames(beta_hat_var) <- colnames(beta_hat_var) <- paste0("beta", 
+    1:J)
+  rownames(conf_interval) <- c(rownames(beta_hat_var), "gamma2")
+  colnames(conf_interval) <- c(quantlow, quantup)
+  r_square <- 1 - colSums((Y - predictor)^2)/sum(Y^2)
+  res <- matrix(Y - predictor, ncol = 2)
+  if (leverage) {
+    Z <- sq_time_lag * grad_mat
+    H <- Z %*% solve(t(Z) %*% Z) %*% t(Z)
+    lever <- diag(H)
+    res <- res/(sqrt(gamma2_hat * (1 - diag(H))))
+  }
+  else {
+    lever <- NULL
+  }
+  AIC <- AICEuler(beta = as.numeric(beta_hat), gamma2 = gamma2_hat, 
+    locs = locs, times = times, ID = ID, grad_array = grad_array)
+  return(list(betaHat = as.numeric(beta_hat), gamma2Hat = gamma2_hat, 
+    betaHatVariance = beta_hat_var, CI = conf_interval, 
+    predicted = matrix(predictor, ncol = 2), R2 = r_square, 
+    residuals = res, lever = lever, AIC = AIC))
+}
+
+# AICEuler
+AICEuler <- function (beta, gamma2 = 1, locs, times, ID = NULL, grad_array) 
+{
+  npar <- length(beta)
+  if (gamma2 != 1) 
+    npar <- npar + 1
+  nllk <- nllkEuler(beta = beta, gamma2 = gamma2, locs = locs, 
+    times = times, ID = ID, grad_array = grad_array)
+  AIC <- 2 * (npar + nllk)
+  return(AIC)
+}
+
+
+# nllkEuler
+nllkEuler <- function (beta, gamma2 = 1, locs, times, ID = NULL, grad_array) 
+{
+  n <- nrow(locs)
+  gradmat <- 0.5 * gamma2 * apply(grad_array, 2, function(mat) mat %*% 
+    beta)
+  if (is.null(ID)) 
+    ID <- rep(1, nrow(locs))
+  break_ind <- which(ID[-1] != ID[-n])
+  start_ind <- c(1, break_ind + 1)
+  end_ind <- c(break_ind, n)
+  dt <- times[-start_ind] - times[-end_ind]
+  llk <- sum(dnorm(locs[-start_ind, ], locs[-end_ind, ] + 
+    dt * gradmat[-end_ind, ], sqrt(gamma2 * dt), log = TRUE))
+  return(-llk)
+}
